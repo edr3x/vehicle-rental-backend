ca48955046a185ef502a08ccc2ffecd88d5bbd63
edr3x <dhunganaannanta@gmail.com>
2023-04-08 12:32:44 +0545

    feat: custom error handler

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,20 @@ import cors from "cors";
 import config from "./config/env";
 
 import { router } from "./routes";
-import { notFound } from "./middlewares/notFound.middleware";
+import {
+    notFound,
+    customErrorHandler,
+} from "./middlewares/error_handlers.middleware";
 
 import { logger } from "./utils/logger";
 
 const app = express();
 
+// middlewares
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use("/api/v1", router);
+app.use(customErrorHandler);
 app.use(notFound);
 
 const port = config.PORT;
--- src/utils/response.ts
+++
@@ -1,7 +0,0 @@
-export function successResponse(data: any) {
-    return { success: true, data };
-}
-
-export function failureResponse(message: any) {
-    return { success: false, message };
-}
