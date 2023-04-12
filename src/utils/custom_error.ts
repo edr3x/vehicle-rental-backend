3f9745f246b4c4c04d0c67e59d2f91ed3c51fb6e
edr3x <dhunganaannanta@gmail.com>
2023-04-12 13:26:30 +0545

    refactor: error handeling

--- src/utils/custom_error.ts
+++ src/utils/custom_error.ts
@@ -1,18 +1,7 @@
-import { Response } from "express";
-import { logger } from "./logger";
-
 export class CustomError extends Error {
     statusCode = 500;
-    message = "Internal Server Error";
     constructor(statusCode: number, message: any) {
-        super();
+        super(message);
         this.statusCode = statusCode;
-        this.message = message;
     }
 }
-
-export const handleError = (err: CustomError, res: Response) => {
-    const { statusCode, message } = err;
-    logger.error(message);
-    res.status(statusCode).json({ success: false, message });
-};
