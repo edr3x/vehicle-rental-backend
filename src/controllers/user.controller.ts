c0727513d9d7435c62871100adc7341661d1ee3a
edr3x <dhunganaannanta@gmail.com>
2023-04-12 14:58:18 +0545

    feat: get User Data

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,10 +3,40 @@ import { NextFunction, Request, Response } from "express";
 import { UpdateUserSchema, UpdateAddressSchema } from "../schemas/user.schema";
 
 import {
+    getAllUserService,
+    getUserService,
     updateAddressService,
     updateUserService,
     uploadImageService,
 } from "../services/user.service";
+
+export async function getUserController(
+    _req: Request,
+    res: Response,
+    next: NextFunction,
+) {
+    try {
+        const response = await getUserService(res.locals.user);
+
+        return res.status(200).json({ success: true, data: response });
+    } catch (e: any) {
+        next(e);
+    }
+}
+
+export async function getAllUsersController(
+    _req: Request,
+    res: Response,
+    next: NextFunction,
+) {
+    try {
+        const response = await getAllUserService();
+
+        return res.status(200).json({ success: true, data: response });
+    } catch (e: any) {
+        next(e);
+    }
+}
 
 export async function updateUserController(
     req: Request<{}, {}, UpdateUserSchema>,
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -3,6 +3,63 @@ import { CustomError } from "../utils/custom_error";
 
 import { UpdateAddressSchema, UpdateUserSchema } from "../schemas/user.schema";
 
+export async function getAllUserService() {
+    const users = await prisma.user.findMany({
+        include: {
+            address: true,
+        },
+    });
+
+    let response: any = [];
+
+    for (let i = 0; i < users.length; i++) {
+        const user = users[i];
+
+        response.push({
+            ...user,
+            phone: Number(user.phone.toString()),
+        });
+    }
+
+    return response;
+}
+
+export async function getUserService(locaUserData: any) {
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                id: locaUserData.id,
+            },
+            select: {
+                fullName: true,
+                phone: true,
+                email: true,
+                gender: true,
+                isProfileUpdated: true,
+                address: {
+                    select: {
+                        province: true,
+                        district: true,
+                        municipality: true,
+                        city: true,
+                        street: true,
+                    },
+                },
+            },
+        });
+        if (!user) {
+            throw new CustomError(400, "User not found");
+        }
+
+        return {
+            ...user,
+            phone: Number(user.phone.toString()),
+        };
+    } catch (e: any) {
+        throw new CustomError(400, e.message);
+    }
+}
+
 export async function updateUserService(
     userDetails: UpdateUserSchema,
     localuserdata: any,
