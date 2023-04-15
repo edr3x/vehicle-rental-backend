5379f740147708d72cc0701ea8d0b358157b387e
edr3x <dhunganaannanta@gmail.com>
2023-04-15 16:31:27 +0545

    refactor: upload middleware

--- src/middlewares/upload.middleware.ts
+++ src/middlewares/upload.middleware.ts
@@ -1,21 +1,19 @@
 import { Request } from "express";
-
-import multer from "multer";
-
 import { join } from "path";
 import { v4 as uuidv4 } from "uuid";
+import multer from "multer";
 
 import { CustomError } from "../utils/custom_error";
 
-const rand = Math.floor(10000 + Math.random() * 90000);
-
 const myStorage = multer.diskStorage({
     destination: (_req: Request, _file: any, cb: any) => {
-        let uploadpath = join(__dirname, "..", "Storage");
+        const uploadpath = join(__dirname, "..", "Storage");
+
         cb(null, uploadpath);
     },
     filename: (_req: Request, file: any, cb: any) => {
-        let filename = uuidv4() + "." + file.originalname.split(".").pop();
+        const filename = uuidv4() + "." + file.originalname.split(".").pop();
+
         cb(null, filename);
     },
 });
@@ -23,11 +21,11 @@ const myStorage = multer.diskStorage({
 export const uploader = multer({
     storage: myStorage,
     fileFilter: (_req: Request, file: any, cb: any) => {
-        let ext_parts = file.originalname.split(".");
-        let ext = ext_parts.pop();
+        const ext_parts = file.originalname.split(".");
+        const ext = ext_parts.pop();
 
         try {
-            let allowed = [
+            const allowed = [
                 "jpg",
                 "jpeg",
                 "png",
@@ -37,13 +35,14 @@ export const uploader = multer({
                 "svg",
                 "pdf",
             ];
+
             if (allowed.includes(ext.toLowerCase())) {
                 cb(null, true);
             } else {
                 cb(null, false);
             }
-        } catch (error) {
-            throw new CustomError(500, "Internal Server Error");
+        } catch (error: any) {
+            throw new CustomError(500, `Image upload error ${error}`);
         }
     },
 });
