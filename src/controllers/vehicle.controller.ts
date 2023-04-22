ccefcdb7222cbe7391a06dc67fbdce2e62fdaeb8
edr3x <dhunganaannanta@gmail.com>
2023-04-22 18:22:22 +0545

    feat: vehicle controllers

--- src/controllers/vehicle.controller.ts
+++ src/controllers/vehicle.controller.ts
@@ -1,7 +1,26 @@
 import { NextFunction, Request, Response } from "express";
 
 import * as VehicleService from "../services/vehicle.service";
-import { AddCategorySchema } from "../schemas/vehicle.schema";
+import {
+    AddBrandSchema,
+    AddCategorySchema,
+    AddSubCategorySchema,
+    AddVehicleSchema,
+} from "../schemas/vehicle.schema";
+
+export async function listAllCategoryController(
+    _req: Request,
+    res: Response,
+    next: NextFunction,
+) {
+    try {
+        const response = await VehicleService.listAllCategory();
+
+        return res.status(200).json({ success: true, data: response });
+    } catch (e: any) {
+        next(e);
+    }
+}
 
 export async function addCategoryController(
     req: Request<{}, {}, AddCategorySchema>,
@@ -11,6 +30,190 @@ export async function addCategoryController(
     try {
         const response = await VehicleService.addCategory(req.body);
 
+        return res.status(201).json({ success: true, data: response });
+    } catch (e: any) {
+        next(e);
+    }
+}
+
+export async function updateCategoryController(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) {
+    try {
+        const response = await VehicleService.updateCategory(
+            req.params.id,
+            req.body,
+        );
+        return res.status(200).json({ success: true, data: response });
+    } catch (e: any) {
+        next(e);
+    }
+}
+
+export async function deleteCategoryController(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) {
+    try {
+        const response = await VehicleService.deleteCategory(req.params.id);
+        return res.status(200).json({ success: true, data: response });
+    } catch (e: any) {
+        next(e);
+    }
+}
+
+export async function addSubCategoryController(
+    req: Request<{}, {}, AddSubCategorySchema>,
+    res: Response,
+    next: NextFunction,
+) {
+    try {
+        const response = await VehicleService.addSubCategory(req.body);
+        return res.status(201).json({ success: true, data: response });
+    } catch (e: any) {
+        next(e);
+    }
+}
+
+export async function updateSubCategoryController(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) {
+    try {
+        const response = await VehicleService.updateSubCategory(
+            req.params.id,
+            req.body,
+        );
+        return res.status(200).json({ success: true, data: response });
+    } catch (e: any) {
+        next(e);
+    }
+}
+
+export async function listAllSubCategoryController(
+    _req: Request,
+    res: Response,
+    next: NextFunction,
+) {
+    try {
+        const response = await VehicleService.listAllSubCategory();
+        return res.status(200).json({ success: true, data: response });
+    } catch (e: any) {
+        next(e);
+    }
+}
+
+export async function deleteSubCategoryController(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) {
+    try {
+        const response = await VehicleService.deleteSubCategory(req.params.id);
+        return res.status(200).json({ success: true, data: response });
+    } catch (e: any) {
+        next(e);
+    }
+}
+
+export async function addBrandController(
+    req: Request<{}, {}, AddBrandSchema>,
+    res: Response,
+    next: NextFunction,
+) {
+    try {
+        const response = await VehicleService.addBrand(req.body);
+        return res.status(201).json({ success: true, data: response });
+    } catch (e: any) {
+        next(e);
+    }
+}
+
+export async function updateBrandController(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) {
+    try {
+        const response = await VehicleService.updateBrand(
+            req.params.id,
+            req.body,
+        );
+        return res.status(200).json({ success: true, data: response });
+    } catch (e: any) {
+        next(e);
+    }
+}
+
+export async function listAllBrandController(
+    _req: Request,
+    res: Response,
+    next: NextFunction,
+) {
+    try {
+        const response = await VehicleService.listAllBrands();
+        return res.status(200).json({ success: true, data: response });
+    } catch (e: any) {
+        next(e);
+    }
+}
+
+export async function deleteBrandController(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) {
+    try {
+        const response = await VehicleService.deleteBrand(req.params.id);
+        return res.status(200).json({ success: true, data: response });
+    } catch (e: any) {
+        next(e);
+    }
+}
+
+export async function addVehicleController(
+    req: Request<{}, {}, AddVehicleSchema>,
+    res: Response,
+    next: NextFunction,
+) {
+    try {
+        const response = await VehicleService.addVehicle(
+            req.body,
+            res.locals.user,
+        );
+        return res.status(201).json({ success: true, data: response });
+    } catch (e: any) {
+        next(e);
+    }
+}
+
+export async function listAllVehicleController(
+    _req: Request,
+    res: Response,
+    next: NextFunction,
+) {
+    try {
+        const response = await VehicleService.listAllVehicle();
+        return res.status(201).json({ success: true, data: response });
+    } catch (e: any) {
+        next(e);
+    }
+}
+
+export async function getVehiclesNearMeController(
+    req: Request<{}, {}, { lat: number; lon: number }>,
+    res: Response,
+    next: NextFunction,
+) {
+    try {
+        const response = await VehicleService.getVehiclesNearMe(
+            req.body.lat,
+            req.body.lon,
+        );
         return res.status(201).json({ success: true, data: response });
     } catch (e: any) {
         next(e);
